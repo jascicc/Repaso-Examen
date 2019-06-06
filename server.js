f08dee7fb580657088a3298d19f65ba3554c0907
jascicc <juan.sotoc.jasc@gmail.com>
2019-06-06 13:42:43 -0700

    Actualizando los estilos, configurando el entry point y creando las vistas.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.static('styles'));
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+app.listen(3000,()=>{
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/tablero',(req, res)=>{
+    res.render('tableroPrincipal.html');
+});
